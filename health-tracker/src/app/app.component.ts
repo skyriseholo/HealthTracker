d25b9534179dded36bc6995747266aabc901da5c
David Walker <skyriseholo@gmail.com>
2025-05-13 22:28:17 -0500

    Set up page to host

--- health-tracker/src/app/app.component.ts
+++ health-tracker/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CalorieWeeklyViewComponent } from './calorie-weekly-view/calorie-weekly-view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalorieWeeklyViewComponent],
+  imports: [ CalorieWeeklyViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
